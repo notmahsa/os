c1b26aaba18ee70cbc0c1d889afd707a2e9726a6
notmahsa <mahsa.naserifar@mail.utoronto.ca>
2019-09-07 15:06:55 -0400

    Added fact

--- warmup/fact.c
+++ warmup/fact.c
@@ -1,8 +1,32 @@
 #include "common.h"
 
-int
-main()
+int factorial(int input){
+    if (input < 3){
+        return input;
+    }
+    return input * factorial(input - 1);
+}
+
+int main(int argc, char **argv)
 {
-	TBD();
+	if (argc != 2){
+	    printf("Huh?");
+	    return 0;
+	}
+
+    int int_input = strtol(argv[1], NULL, 10);
+    if (int_input > 12){
+        printf("Overflow");
+        return 0;
+    }
+
+	float dec_input = atof(argv[1]);
+    if (int_input < 1 || dec_input != int_input){
+        printf("Huh?");
+        return 0;
+    }
+
+    printf("%d", factorial(int_input));
+
 	return 0;
 }
--- warmup/words.c
+++ warmup/words.c
@@ -3,6 +3,11 @@
 int
 main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        return 0;
+    }
+
 	for (int i = 1; i < argc; ++i)
     {
         printf("%s\n", argv[i]);
