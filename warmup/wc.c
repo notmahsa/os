f9291b31424f1dec809bedf662aa5c9e314ac8e6
notmahsa <mahsa.naserifar@mail.utoronto.ca>
2019-09-08 09:05:38 -0400

    Added wc, no destroy yet

--- warmup/wc.c
+++ warmup/wc.c
@@ -1,11 +1,15 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
 #include "common.h"
+#include "hashtable.h"
 #include "wc.h"
 
 struct wc {
-	/* you can define this struct to have whatever fields you want. */
+	char *word_array;
+	long int size;
 };
 
 struct wc *
@@ -16,20 +20,45 @@ wc_init(char *word_array, long size)
 	wc = (struct wc *)malloc(sizeof(struct wc));
 	assert(wc);
 
-	TBD();
-
+    wc->word_array = strdup(word_array);
+    wc->size = size;
 	return wc;
 }
 
 void
 wc_output(struct wc *wc)
 {
-	TBD();
+    HashTable table;
+    ht_setup(&table, sizeof(int), sizeof(char*), 10);
+    char * word_array_copy = strdup(wc->word_array);
+
+    char * token;
+    token = strtok(word_array_copy, " ");
+    int i = 0;
+    int new_val;
+    while (token != NULL)
+    {
+        new_val = 1;
+        if (ht_contains(&table, &token))
+            new_val += *(int*)ht_lookup(&table, &token);
+
+        ht_insert(&table, &token, &new_val);
+        i++;
+        token = strtok(NULL, " ");
+    }
+
+    for (int i = 0; i < table.size; i++){
+        char * keyword = *(char**)(table.nodes[i]->key);
+        printf("%s: %d", keyword, *(int*)ht_lookup(&table, &keyword));
+    }
+
+	ht_clear(&table);
+    ht_destroy(&table);
 }
 
 void
 wc_destroy(struct wc *wc)
 {
-	TBD();
+	//TBD();
 	free(wc);
 }
