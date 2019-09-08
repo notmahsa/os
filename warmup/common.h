09af4b53fc58c34ef0061644e2c997f0d7829320
notmahsa <mahsa.naserifar@mail.utoronto.ca>
2019-09-08 15:09:48 -0400

    Added single file cpr

--- warmup/common.h
+++ warmup/common.h
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 #define TBD() do {							\
 		printf("%s:%d: %s: please implement this functionality\n", \
--- warmup/cpr.c
+++ warmup/cpr.c
@@ -1,4 +1,10 @@
 #include "common.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* make sure to use syserror() when a system call fails. see common.h */
 
@@ -15,6 +21,23 @@ main(int argc, char *argv[])
 	if (argc != 3) {
 		usage();
 	}
-	TBD();
+
+	char buffer[1024];
+    int files[2];
+    size_t count;
+
+    files[0] = open(argv[1], O_RDONLY);
+    if (files[0] == -1) /* Check if file opened */
+        syserror(open, argv[1]);
+        return -1;
+    files[1] = open(argv[2], O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR);
+    if (files[1] == -1) /* Check if file opened (permissions problems ...) */
+    {
+        close(files[0]);
+        return -1;
+    }
+
+    while ((count = read(files[0], buffer, sizeof(buffer))) != 0)
+        write(files[1], buffer, count);
 	return 0;
 }
