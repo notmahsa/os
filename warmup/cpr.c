727ab3fb5d22910ad9590c4224c4601e300a75bf
notmahsa <mahsa.naserifar@mail.utoronto.ca>
2019-09-09 11:45:34 -0400

    Added folder recursive copy

--- warmup/cpr.c
+++ warmup/cpr.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <stdbool.h>
-#include <ftw.h>
 #include <libgen.h>
 
 /* make sure to use syserror() when a system call fails. see common.h */
@@ -20,7 +19,7 @@ usage()
 }
 
 int
-copy_file(char* location, char* destination){
+copy_file(char * location, const char * destination){
     char buf[4096];
     int infile;
     int outfile;
@@ -51,57 +50,8 @@ copy_file(char* location, char* destination){
     return 0;
 }
 
-void
-list_dir(const char *location, int indent)
-{
-    DIR *dir;
-    struct dirent *entry;
-
-    if (!(dir = opendir(location))){
-        syserror(opendir, location);
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_DIR) {
-            char buf[1024];
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-                continue;
-            snprintf(buf, sizeof(buf), "%s/%s", location, entry->d_name);
-            printf("%*s[%s]\n", indent, "", entry->d_name);
-            list_dir(buf, indent + 2);
-        } else {
-            printf("%*s- %s  ---  %s\n", indent, "", entry->d_name, location);
-        }
-    }
-    closedir(dir);
-}
-
-//void
-//copy_dir(const char *location, const char *destination)
-//{
-//    DIR *dir;
-//    struct dirent *entry;
-//
-//    if (!(dir = opendir(location))){
-//        syserror(opendir, location);
-//    }
-//
-//    while ((entry = readdir(dir)) != NULL) {
-//        if (entry->d_type == DT_DIR) {
-//            char buf[1024];
-//            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-//                continue;
-//            snprintf(buf, sizeof(buf), "%s/%s", location, entry->d_name);
-//            printf("%*s[%s]\n", indent, "", entry->d_name);
-//            list_dir(buf, indent + 2);
-//        } else {
-//            printf("%*s- %s  ---  %s\n", indent, "", entry->d_name, location);
-//        }
-//    }
-//    closedir(dir);
-//}
-
-struct stat * get_buf(char * location)
+struct stat *
+get_stat(const char * location)
 {
     struct stat * buf;
     buf = (struct stat *)malloc(sizeof(struct stat));
@@ -114,7 +64,55 @@ struct stat * get_buf(char * location)
     return buf;
 }
 
-bool is_file(struct stat * buf){
+char *
+make_dir(const char *destination, const char *foldername, const mode_t mode){
+    char * buf = malloc(sizeof(char) * 16384);
+    snprintf(buf, sizeof(char) * 16384, "%s/%s", destination, foldername);
+    //printf("Creating folder %s, dest=%s, foldername=%s\n", buf, destination, foldername);
+    if (mkdir(buf, mode) != 0){
+        if (errno == EEXIST){
+            //printf("Folder %s already exists", buf);
+            return buf;
+        }
+        syserror(mkdir, buf);
+    }
+    return buf;
+}
+
+void
+copy_dir(const char *location, const char *destination, int indent)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(location))){
+        syserror(opendir, location);
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        char buf[16384];
+        snprintf(buf, sizeof(char) * 16384, "%s/%s", location, entry->d_name);
+        if (entry->d_type == DT_DIR) {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+            //printf("%*s[%s]\n", indent, "", entry->d_name);
+
+            char * created_dir;
+            struct stat * loc_stat = get_stat(location);
+            created_dir = make_dir(destination, entry->d_name, loc_stat->st_mode);
+            free(loc_stat);
+
+            copy_dir(buf, created_dir, indent + 2);
+        } else {
+            //printf("%*s- %s  ---  %s\n", indent, "", entry->d_name, location);
+            copy_file(buf, destination);
+        }
+    }
+    closedir(dir);
+}
+
+bool
+is_file(struct stat * buf){
     bool verdict;
     switch (buf->st_mode & S_IFMT) {
         case S_IFDIR:  verdict = false; break;
@@ -130,7 +128,7 @@ main(int argc, char *argv[])
 		usage();
 	}
     struct stat * buf;
-    buf = get_buf(argv[1]);
+    buf = get_stat(argv[1]);
     if (is_file(buf)){
         if (copy_file(argv[1], argv[2]) != 0){
             free(buf);
@@ -138,7 +136,9 @@ main(int argc, char *argv[])
         }
     }
     else {
-        list_dir(argv[1], 8);
+        char * created_dir;
+        created_dir = make_dir(argv[2], basename(argv[1]), buf->st_mode);
+        copy_dir(argv[1], created_dir, 8);
     }
     free(buf);
 }
