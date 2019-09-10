348072cf222e509e0d3be653e32f98e9da4fd2f5
notmahsa <mahsa.naserifar@mail.utoronto.ca>
2019-09-09 22:50:10 -0400

    Added post perm

--- warmup/cpr.c
+++ warmup/cpr.c
@@ -65,7 +65,6 @@ copy_file(char * location, const char * destination){
     close(infile);
     close(outfile);
     int out;
-    mode_t temp_mode;
     if ((out = chmod(full_file, get_stat(location)->st_mode) != 0)){
         syserror(chmod, full_file);
     }
