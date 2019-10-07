93c126624063cdb4797dece88a30c32ac7fc7c0c
Mahsa Naserifar <mahsa@render.com>
2019-10-07 14:33:28 -0400

    Removing all instances in pop

--- threads/thread.c
+++ threads/thread.c
@@ -233,6 +233,7 @@ thread_yield(Tid want_tid)
     running->state = 1;
     // running->context->uc_mcontext.gregs[REG_RIP] = new_context.uc_mcontext.gregs[REG_RIP];
 
+    thread_pop_from_ready_queue(running->id);
     thread_append_to_ready_queue(running->id);
 
     if (want_tid == THREAD_ANY){
