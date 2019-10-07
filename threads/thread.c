0007bfe97f69c71939b37361c46820389ca33188
Mahsa Naserifar <mahsa@render.com>
2019-10-07 19:21:26 -0400

    Changed to check for false

--- threads/thread.c
+++ threads/thread.c
@@ -194,7 +194,7 @@ thread_create(void (*fn) (void *), void *parg)
     short unsigned int new_id = -1;
     for (int i = 1; i < THREAD_MAX_THREADS; i++){
         if (threads_exist[i] == false){
-            printf("id %d in max %d", i, THREAD_MAX_THREADS);
+            printf("id %d in max %d\n", i, THREAD_MAX_THREADS);
             new_id = i;
             break;
         }
